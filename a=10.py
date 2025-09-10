da433fa6e7ab91fd6f91ea326e329951f290929b
Abistackly <abirami14297@gmail.com>
2025-09-10 14:35:39 +0530

    first commit

---
+++ a=10.py
@@ -0,0 +1,98 @@
+a=20
+b=30
+
+#Arithmic operators
+
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+print (a//b)
+print (a%b)
+print (a**b)
+
+
+#Relational operators
+
+print (a==b)
+print (a!=b)
+print (a<b)
+print (a>b)
+print (a<=b)
+print (a>=b)
+
+
+#Logical operators
+
+print (a>9 and b<10)
+print (a>9 or b<10)
+not(a>9)
+not(b<10)
+
+
+
+#Assignment operators
+
+a=20
+b=8
+a+=b
+print(a)
+
+a=20
+b=8
+a-=b
+print(a)
+
+a=20
+b=8
+a*=b
+print(a)
+
+a=20
+b=8
+a/=b
+print(a)
+
+a=20
+b=8
+a//=b
+print(a)
+
+a=20
+b=8
+a**=b
+print(a)
+
+a=20
+b=8
+a%=b
+print(a)
+
+
+#Bitwise operators
+
+a=20
+b=8
+print(a&b)
+
+
+a=10
+b=8
+print(a|b)
+
+a=20
+b=8
+print(a^b)
+
+a=10
+b=8
+print(a<<2)
+
+a=10
+b=8
+print(a>>2)
+
+a=10
+b=8
+print(~a)
+
